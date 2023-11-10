66c766e3c450b9573f8c39d575e96c41ccc94d27
fabiansingler <149794374+fabiansingler@users.noreply.github.com>
2023-11-10 10:29:56 +0100

    Aenderungen_002

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -6,6 +6,7 @@
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,19 +15,25 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Variable count als Integer");
         app.parse(argc, argv);
+
+        
+
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int>data(count);
 
+    fmt::print("Created a vector with {} elements\n", data.size());
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Hello, {}!\n", count, app.get_name());
 
     /* INSERT YOUR CODE HERE */
 
